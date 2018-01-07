4b86b7f39b13eac8cb1d563bbf9a49458477e58f
Kenya Kawatsu <3982ne@gmail.com>
2018-01-08 03:03:40 +0900

    余分なファイルの削除

--- scripts/gmaps.js
+++
@@ -1,8 +0,0 @@
-const geo = new TimeManagerGeolocation();
-geo.getPosition()
-    .then((position) => {
-        var mapDiv = document.getElementById("map_div");
-        geo.initMap(mapDiv);
-    })
-    .catch((reason) => console.error(reason));
-    
